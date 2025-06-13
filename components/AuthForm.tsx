0a392ec8d7fb48e3586da73d513567fd3c3a295f
Joe Stephen K <joe.stephen@oneteamus.com>
2025-06-13 18:06:19 +0530

    Added: sign up base.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,13 +5,14 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import FormField from "./FormField";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-} from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
 import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signUp } from "@/lib/actions/auth.aciton";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -33,9 +34,25 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
+        const { name, email, password } = values;
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const result: any = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+        if (!result?.success) {
+          toast.error(result.message);
+          return;
+        }
         toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       } else {
--- lib/actions/auth.aciton.ts
+++ lib/actions/auth.aciton.ts
@@ -1,8 +1,24 @@
 "use server";
+
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+//variable declaration
+const ONE_WEEK = 60 * 60 * 24 * 7 * 1000;
+
 export async function signUp(params: SignUpParams) {
   try {
     const { uid, name, email } = params;
+    //check if user already exists
     const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists) {
+      return { success: false, message: "This email is already in use." };
+    }
+    //creating a new user
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
   } catch (error: any) {
     console.error("Error while signing up:", error);
     if (error.code === "auth/email-already-exists.") {
@@ -10,4 +26,18 @@ export async function signUp(params: SignUpParams) {
     }
     return { success: false, message: "Failed to create an account." };
   }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK, // 1 week
+  });
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    path: "/",
+    sameSite: "lax",
+  });
 }
