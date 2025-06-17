87bef8518d67c2e6365d19573946d14be186b23a
Joe Stephen K <joe.stephen@oneteamus.com>
2025-06-17 18:29:45 +0530

    Update: signUp and signIn added.

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,9 +10,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "@/firebase/client";
-import { signUp } from "@/lib/actions/auth.aciton";
+import { signIn, signUp } from "@/lib/actions/auth.aciton";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -56,6 +59,18 @@ const AuthForm = ({ type }: { type: FormType }) => {
         toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       } else {
+        const { email, password } = values;
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in failed. Please try again.");
+          return;
+        }
+        await signIn({ email, idToken });
         toast.success("Signed in successfully.");
         router.push("/");
       }
--- lib/actions/auth.aciton.ts
+++ lib/actions/auth.aciton.ts
@@ -19,12 +19,33 @@ export async function signUp(params: SignUpParams) {
       name,
       email,
     });
+    return {
+      success: true,
+      message: "Account created successfully. Please sign in",
+    };
   } catch (error: any) {
     console.error("Error while signing up:", error);
     if (error.code === "auth/email-already-exists.") {
       return { success: false, message: "This email is already in use." };
     }
     return { success: false, message: "Failed to create an account." };
+  }
+}
+
+export async function signIn(params: SignInParams) {
+  try {
+    const { email, idToken } = params;
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exist. Create an account instead.",
+      };
+    }
+    await setSessionCookie(idToken);
+  } catch (error: any) {
+    console.error("Error while signing in:", error);
+    return { success: false, message: "Failed to sign in." };
   }
 }
 
